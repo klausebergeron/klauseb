35c16672731b5b160d889ed0d7e38a7d52a1d0b1
klausebergeron <klausebergeron@gmail.com>
2024-02-24 15:27:32 -0500

    start of homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react'
+import logo from './assets/klause-b-logo.png';
+import resume from './assets/Resume.pdf';
+import lowerIcon from './assets/lower-icon.png';
+import './styles/App.less'
+import './styles/common.css'
 
 function App() {
+  const [resumeOpen, setResumeOpen] = useState(false)
+
+  const handleResumeClose = () => {
+    
+  }
   return (
     <>
-      <h1>Site in progress</h1>
+      <img src={logo} style={{ width: "22%"}}/>
+      <div className={'buttonRow col-6'}>
+        <div className={'button resume'} onClick={() => setResumeOpen(!resumeOpen)}>resume</div>
+        <div className={'button art'}>Art</div>
+        <div className={'button writing'}>Writing</div>
+        <div className={'button research'}>research</div>
+      </div>
+      {resumeOpen && 
+      <>
+        <iframe className={'resume'} src = {resume} id="resume"></iframe>
+        <div className={'closeResume'}>
+          <img src={lowerIcon}/>
+        </div>
+      </>
+      }
     </>
   )
 }
