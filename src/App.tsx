430925508a17286e6d8464154b684017ea005a63
klausebergeron <klausebergeron@gmail.com>
2024-02-24 17:29:49 -0500

    Homepage buttons, resume pop-up

--- src/App.tsx
+++ src/App.tsx
@@ -6,28 +6,25 @@ import './styles/App.less'
 import './styles/common.css'
 
 function App() {
-  const [resumeOpen, setResumeOpen] = useState(false)
+  const [resumeOpen, setResumeOpen] = useState(true);
 
-  const handleResumeClose = () => {
-    
-  }
   return (
     <>
       <img src={logo} style={{ width: "22%"}}/>
       <div className={'buttonRow col-6'}>
-        <div className={'button resume'} onClick={() => setResumeOpen(!resumeOpen)}>resume</div>
+        <div className={'button resume'} onClick={() => setResumeOpen(true)}>resume</div>
         <div className={'button art'}>Art</div>
         <div className={'button writing'}>Writing</div>
         <div className={'button research'}>research</div>
       </div>
-      {resumeOpen && 
-      <>
-        <iframe className={'resume'} src = {resume} id="resume"></iframe>
-        <div className={'closeResume'}>
-          <img src={lowerIcon}/>
+      {resumeOpen &&
+        <div className={'resumeWrapper'} style={{ width: '65%'}}>
+          <iframe className={'resume'} src = {resume} id="resume"></iframe>
+          <div className={'closeResume'} onClick={() => setResumeOpen(false)}>
+            <img src={lowerIcon}/>
+          </div>
         </div>
-      </>
-      }
+}
     </>
   )
 }
