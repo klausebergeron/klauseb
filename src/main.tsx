ca8129cb0fca949a471d5ca534f0cfc17ffcebdc
klausebergeron <klausebergeron@gmail.com>
2024-04-11 13:56:07 -0400

    Move style imports to main

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "./styles/app.less";
+import "./styles/common.less";
 import { Amplify } from "aws-amplify";
 import amplifyconfig from "../src/amplifyconfiguration.json";
 Amplify.configure(amplifyconfig);
