a8c166b7fb59e5beec3baa1d4bd2c1e739ca48f2
klausebergeron <klausebergeron@gmail.com>
2024-04-11 17:40:09 -0400

    Fix artwork title parsing

--- src/ArtPage/PictureContainer.tsx
+++ src/ArtPage/PictureContainer.tsx
@@ -7,9 +7,10 @@ const PictureContainer: React.FC<PictureContainerProps> = (props) => {
   const fpArr = filePath.split("/");
   console.log("filepath: ", filePath.split("/"));
   var title = fpArr[fpArr.length - 1];
-  title = title.includes("-D")
-    ? title.slice(0, title.indexOf("-"))
-    : title.replace(".jpg", "").replace(".png", "");
+  title =
+    title.includes(".jpg") || title.includes(".png")
+      ? title.replace(".jpg", "").replace(".png", "")
+      : title.slice(0, title.indexOf("-"));
   return (
     <div className="picture-container col-6 col-s-8">
       <img className="image" src={filePath} />
