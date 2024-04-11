eeb2c03354020d357ca695bd7e40d79bde6ffbdc
klausebergeron <klausebergeron@gmail.com>
2024-04-11 13:26:01 -0400

    Update root dir

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
-import amplifyconfig from "./amplifyconfiguration.json";
+import amplifyconfig from "../src/amplifyconfiguration.json";
 Amplify.configure(amplifyconfig);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
