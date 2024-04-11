d965208ebf3fa627c70a26e623be47729200da87
klausebergeron <klausebergeron@gmail.com>
2024-04-11 19:13:29 -0400

    Clean up and capitalize artwork titles

--- src/ArtPage/PictureContainer.tsx
+++ src/ArtPage/PictureContainer.tsx
@@ -5,15 +5,11 @@ interface PictureContainerProps {
 const PictureContainer: React.FC<PictureContainerProps> = (props) => {
   const { filePath } = props;
   const fpArr = filePath.split("/");
-  console.log("filepath: ", filePath.split("/"));
   var title = fpArr[fpArr.length - 1]
     .replace(".jpg", "")
     .replace(".png", "")
     .replaceAll("%20", " ");
   const indToSlice = title.includes("-") ? title.indexOf("-") : title.length;
-  title.slice(0, indToSlice);
-  console.log("title: ", title);
-  console.log("title slice: ", title.slice(0, indToSlice));
   return (
     <div className="picture-container col-6 col-s-8">
       <img className="image" src={filePath} />
