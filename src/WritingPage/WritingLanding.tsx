0448166167388663785217704bcde2dbfa7753a3
klausebergeron <klausebergeron@gmail.com>
2024-04-16 17:15:16 -0400

    Slightly more responsive writing page styles

--- src/WritingPage/WritingLanding.tsx
+++ src/WritingPage/WritingLanding.tsx
@@ -19,31 +19,33 @@ const WritingLanding: React.FC = () => {
       <div style={{ marginTop: "80px" }}>
         <h3 className={"show-mobile"}>Under construction</h3>
         <h4 className={"show-mobile"}>Responsive styling in progress</h4>
-        <div id="circleSpace">
-          <div id="centercircle">
-            <h1 className="titleW">Writing</h1>
+        <div id="circleSpaces">
+          <div id="circleSpace">
+            <div id="centercircle">
+              <h1 className="titleW">Writing</h1>
+            </div>
+            <div className="planet planet1" />
+            <div className="planet planet1 delay"></div>
+            <div className="subtitleW one">Short Stories</div>
+            <div className="planet planet2" />
+            <div className="planet planet2 delay"></div>
+            <div className="subtitleW two">Poems</div>
+            <div className="planet planet3" />
+            <div className="planet planet3 delay"></div>
+            <div className="subtitleW three">Book Chapters</div>
+            <div className="planet planet4" />
+            <div className="planet planet4 delay"></div>
+            <div className="subtitleW four">Papers</div>
           </div>
-          <div className="planet planet1" />
-          <div className="planet planet1 delay"></div>
-          <div className="subtitleW one">Short Stories</div>
-          <div className="planet planet2" />
-          <div className="planet planet2 delay"></div>
-          <div className="subtitleW two">Poems</div>
-          <div className="planet planet3" />
-          <div className="planet planet3 delay"></div>
-          <div className="subtitleW three">Book Chapters</div>
-          <div className="planet planet4" />
-          <div className="planet planet4 delay"></div>
-          <div className="subtitleW four">Papers</div>
-        </div>
-        <div id="miniCircleSpace">
-          {pages.map((p) => {
-            return (
-              <div className="miniPlanet" id={p.replace(" ", "")}>
-                <Link to={"/writing/pages/" + p}>{p}</Link>
-              </div>
-            );
-          })}
+          <div id="miniCircleSpace">
+            {pages.map((p) => {
+              return (
+                <div className="miniPlanet" id={p.replace(" ", "")}>
+                  <Link to={"/writing/pages/" + p}>{p}</Link>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
